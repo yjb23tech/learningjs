f07f95f2369fea666a06801c593741ed9e07cef5
yjb23tech <yjb23tech@gmail.com>
2024-01-29 17:21:57 +0000

    Knowing what I be doing

---
+++ basic_maths.js
@@ -0,0 +1,11 @@
+let x = 1 
+let y = 2 
+
+function sumAdd(int_a, int_b) {
+    let sumResult = int_a + int_b
+    return sumResult
+}
+
+const outputResult = sumAdd(x, y)
+
+console.log(outputResult)
