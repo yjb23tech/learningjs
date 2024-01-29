29f431d5d70258e43e1634943327b8bac29b9101
yjb23tech <yjb23tech@gmail.com>
2024-01-29 19:01:56 +0000

    drycleaning

--- basic_for_loop.js
+++ basic_for_loop.js
@@ -1,11 +1,10 @@
-const prompt = require('prompt-sync')(); 
-
 let pandoraMusic = ['American Dreaming', 'H To The Izzo', '99 Problems']
 
 for (let i = 0; i < pandoraMusic.length; i++) {
     console.log(pandoraMusic[i])
 }
 
-let user_name = prompt("What is your name?\n")
+const prompt = require('prompt-sync')(); 
+const user_name = prompt("What is your name?")
 console.log(`Your name is ${user_name}`)
 
